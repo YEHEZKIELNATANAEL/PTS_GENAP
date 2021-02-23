779a9035410a16c4245a180d7206ba9ee0663b97
Yehezkiel natanael <78515391+YEHEZKIELNATANAEL@users.noreply.github.com>
2021-02-23 13:53:43 +0700

    TAMBAH SESUATU
    tambah form contact.php, fieldset dan legend, input nama dan tombol submit

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>github dan html</title>
+</head>
+<body>
+<h1>Github dan HTML</h1>
+<form action=&quot;contact.php&quot; method=&quot;POST&quot;>
+<fieldset>
+<legend>Registrasi&lt</legend>
+<p>
+<label>Nama:</label>
+<input type=&quot;text&quot; name=&quot;nama&quot; placeholder=&quot;Nama lengkap...&quot; >
+<p>
+<input type=&quot;submit&quot; name=&quot;submit&quot; value=&quot;Daftar&quot; >
+</p>
+</fieldset>
+</form>
+</body>
+</html>
